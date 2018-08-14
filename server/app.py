0fdebd8e389e0a9a490ff347e3bbc0c26817834d
nobodyishere <42330530+suledonat@users.noreply.github.com>
2018-08-13 21:42:02 -0400

    asddfasdof

--- server/app.py
+++ server/app.py
@@ -15,6 +15,13 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+player_set = PlayerSet()
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+player_set.load_projection_stats_DB(conn)
+r = redis.from_url(os.environ.get("REDIS_URL"))
+r.set('projections_json', json.dumps(player_set.get_all(), cls=FullPlayerJsonEncoder))
+
 
 DEFAULTS = {
     "num_teams": 12,
@@ -117,10 +124,4 @@ def upload_projectionsfromdb():
 
 @app.route('/')
 def index():
-    player_set = PlayerSet()
-    DATABASE_URL = os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    player_set.load_projection_stats_DB(conn)
-    r = redis.from_url(os.environ.get("REDIS_URL"))
-    r.set('projections_json', json.dumps(player_set.get_all(), cls=FullPlayerJsonEncoder))
     return render_template('index.html')
