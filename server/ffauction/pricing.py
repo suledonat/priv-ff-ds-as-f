b01516871ee339aaa6ab832683d3c485349498a7
nobodyishere <42330530+suledonat@users.noreply.github.com>
2019-09-02 23:32:25 -0400

    change px

--- server/ffauction/pricing.py
+++ server/ffauction/pricing.py
@@ -40,13 +40,11 @@ class VBDModel:
                 if not numpy.isclose(avg_sos,0):
                     scalar = player.sos/avg_sos
                 new_vbd = (player.projected_points - pos_base_vbd)
-                sgn = 1.0
                 if new_vbd < 0.0:
-                    sgn = -1.0
                     if not assign_negative_vbd:
                         new_vbd = 0.0
 
-                new_vbd = (new_vbd ** 2) * (scalar) * sgn
+                new_vbd = new_vbd * scalar 
 
                 setattr(player, target_field, new_vbd)
 
@@ -75,8 +73,7 @@ class PriceModel:
             max_price = 1.0e6
             if player.position in dollar_caps:
                 max_price = dollar_caps[player.position]
-            #player.base_price = max((player.starter_vbd * starter_pf + (player.bench_vbd - player.starter_vbd)* bench_pf),max_price)
-            player.base_price = max((player.starter_vbd * starter_pf + (player.bench_vbd - player.starter_vbd)* bench_pf),max_price)
+            player.base_price = ((player.starter_vbd * starter_pf) + (numpy.sqrt(max((player.bench_vbd - player.starter_vbd)* starter_pf,0))) + numpy.sqrt(max((player.bench_vbd - player.starter_vbd)* bench_pf,0)))
 
         return (starter_pf, bench_pf)
 
