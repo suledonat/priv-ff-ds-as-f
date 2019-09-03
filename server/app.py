a4c9dc92c48d80d69ea6c073766235d604d3572a
nobodyishere <42330530+suledonat@users.noreply.github.com>
2019-09-02 21:40:54 -0400

    ugh

--- server/app.py
+++ server/app.py
@@ -100,7 +100,7 @@ def get_players():
             'players': league.player_set.get_all()
             }, cls=PlayerPriceJsonEncoder)
     except Exception as e: 
-        return str(e) + "/r/n" +str(err.__traceback__)
+        return str(e) + "/r/n" +str(e.__traceback__)
 
 
 #def upload_projections():
